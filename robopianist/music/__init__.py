497b7b3047a4a2fdfc42b990a1bc43347ecb4c5f
zjuzhoutian <128466254+3190100993@users.noreply.github.com>
2025-06-17 15:07:20 +0800

    Add files via upload

--- robopianist/music/__init__.py
+++ robopianist/music/__init__.py
@@ -12,54 +12,99 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Music module."""
+
 from pathlib import Path
+from typing import Union
 
-__version__ = "1.0.10"
+from robopianist import _PROJECT_ROOT
+from robopianist.music import library, midi_file
 
-# Path to the root of the project.
-_PROJECT_ROOT = Path(__file__).parent.parent
 
-# Path to the soundfont directory.
-_SOUNDFONT_PATH = _PROJECT_ROOT / "robopianist" / "soundfonts"
+def _camel_case(name: str) -> str:
+    new_name = name.replace("'", "")  # Remove apostrophes.
+    new_name = new_name.replace("_", " ").title().replace(" ", "")
+    # We have a -{number} suffix which originally came from the different fingering
+    # annotations per file in the PIG dataset. We remove it here.
+    if "-" in new_name:
+        new_name = new_name[: new_name.index("-")]
+    return new_name
 
-# TimGM6mb.sf2 is the default soundfont file that is packaged with the pip install or
-# available when installing from source using `bash scripts/install_deps.sh`.
-_DEFAULT_SF2_PATH = _SOUNDFONT_PATH / "TimGM6mb.sf2"
 
-# We first check if the user has a .robopianistrc file in their home directory. If so,
-# we check if it specifies a soundfont file. If so, we use that.
-_RC_FILE = Path.home() / ".robopianistrc"
-if _RC_FILE.exists():
-    found = False
-    with open(_RC_FILE, "r") as f:
-        for line in f:
-            if line.startswith("DEFAULT_SOUNDFONT="):
-                soundfont_path = line.split("=")[1].strip()
-                SF2_PATH = _SOUNDFONT_PATH / f"{soundfont_path}.sf2"
-                if not SF2_PATH.exists():
-                    SF2_PATH = _DEFAULT_SF2_PATH
-                found = True
-                break
-    if not found:
-        SF2_PATH = _DEFAULT_SF2_PATH
-# Otherwise, we look in the soundfont directory. Our preference is for the higher
-# quality SalamanderGrandPiano.sf2, but if that is not found, we fall back to the
-# default soundfont file.
-else:
-    _SALAMANDER_SF2_PATH = _SOUNDFONT_PATH / "SalamanderGrandPiano.sf2"
-    if _SALAMANDER_SF2_PATH.exists():
-        SF2_PATH = _SALAMANDER_SF2_PATH
+_PIG_DIR = _PROJECT_ROOT / "robopianist" / "music" / "data" / "pig_single_finger"
+_PIG_FILES = sorted(_PIG_DIR.glob("*.proto"))
+PIG_MIDIS = [_camel_case(Path(f).stem) for f in _PIG_FILES]
+_ETUDE_SUBSET = (
+    "french_suite_no_1_allemande-1",
+    "french_suite_no_5_sarabande-1",
+    "piano_sonata_d_845_1st_mov-1",
+    "partita_no_2_6-1",
+    "waltz_op_64_no_1-1",
+    "bagatelle_op_3_no_4-1",
+    "kreisleriana_op_16_no_8-1",
+    "french_suite_no_5_gavotte-1",
+    "piano_sonata_no_23_2nd_mov-1",
+    "golliwogg's_cakewalk-1",
+    "piano_sonata_no_2_1st_mov-1",
+    "piano_sonata_k_279_in_c_major_1st_mov-1",
+)
+ETUDE_MIDIS = [_camel_case(name) for name in _ETUDE_SUBSET]
+_PIG_NAME_TO_FILE = dict(zip(PIG_MIDIS, _PIG_FILES))
+DEBUG_MIDIS = list(library.MIDI_NAME_TO_CALLABLE.keys())
+ALL = DEBUG_MIDIS + PIG_MIDIS
+
+
+def load(
+    path_or_name: Union[str, Path],
+    stretch: float = 1.0,
+    shift: int = 0,
+) -> midi_file.MidiFile:
+    """Make a MidiFile object from a path or name.
+
+    Args:
+        path_or_name: Path or name of the midi file.
+        stretch: Temporal stretch factor. Values greater than 1.0 slow down a song, and
+            values less than 1.0 speed it up.
+        shift: Number of semitones to transpose the song by.
+
+    Returns:
+        A MidiFile object.
+
+    Raises:
+        ValueError if the path extension is not supported or the MIDI file is invalid.
+        KeyError if the name is not found in the library.
+    """
+    path = Path(path_or_name)
+
+    if path.suffix:  # Note strings will have an empty string suffix.
+        midi = midi_file.MidiFile.from_file(path)
     else:
-        if not _DEFAULT_SF2_PATH.exists():
-            raise FileNotFoundError(
-                f"The default soundfont file {_DEFAULT_SF2_PATH} does not exist. Make "
-                "sure you have first run `bash scripts/install_deps.sh` in the root of "
-                "the project directory."
-            )
-        SF2_PATH = _DEFAULT_SF2_PATH
+        # Debug midis are generated programmatically and thus should not be loaded from
+        # file.
+        if path.stem in DEBUG_MIDIS:
+            midi = library.MIDI_NAME_TO_CALLABLE[path.stem]()
+        # PIG midis are stored as proto files and should be loaded from file.
+        elif path.stem in PIG_MIDIS:
+            midi = midi_file.MidiFile.from_file(_PIG_NAME_TO_FILE[path.stem])
+        else:
+            raise KeyError(f"Unknown name: {path.stem}. Available names: {ALL}.")
+
+    return midi.stretch(stretch).transpose(shift)
 
 
 __all__ = [
-    "__version__",
-    "SF2_PATH",
+    "ALL",
+    "DEBUG_MIDIS",
+    "PIG_MIDIS",
+    "ETUDE_MIDIS",
+    "load",
 ]
+
+# print("All registered environments:")
+# for env_name in ALL:
+#     print(env_name)
+    # try:
+    #     env = load(environment_name=env_name)
+    #     print(f"Successfully loaded environment: {env_name}")
+    # except Exception as e:
+    #     print(f"Failed to load environment {env_name}: {e}")
